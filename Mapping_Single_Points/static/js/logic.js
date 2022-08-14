0fa3193b42cd14b6635cc22640d05fe3055335bb
Stella <stellalimkim@gmail.com>
2022-08-13 18:38:36 -0700

    updates to single point map

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,13 +2,29 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-// We create the tile layer that will be the background of our map.
+// Add markers on map
+// Los Angeles
+// marker
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// circle
+L.circle([34.0522, -118.2437], {
+    radius: 300,
+    color: 'black',
+    fillColor: 'yellow',
+    fillOpacity: 0.2
+ }).addTo(map);
+ // another way to do circle
+//  L.circleMarker([34.0522, -118.2437]).addTo(map);
+
+
+
+// Create the tile layer that will be the background of the map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
