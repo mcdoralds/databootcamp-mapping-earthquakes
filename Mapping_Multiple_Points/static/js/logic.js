cf18f086521d2e97a68cf56a65c7cc708d8c3d9a
Stella <stellalimkim@gmail.com>
2022-08-14 22:26:01 -0700

    update Mapping_Multiple_Points

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,7 +2,44 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([35.0522, -95.2437], 5);
+// let map = L.map('mapid').setView([35.0522, -95.2437], 5);
+
+// Create map object for San Fran
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+
+// Add GeoJSON data for San Francisco Airport
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+  // We turn each feature into a marker on the map.
+  pointToLayer: function(feature, latlng) {
+    console.log(feature);
+    return L.marker(latlng)
+    .bindPopup("<h2>" + feature.properties.city + "</h2>")
+
+  }
+  
+
+}).addTo(map);
 
 // Add markers on map
 // An array containing each city's location, state, and population.
@@ -54,18 +91,6 @@ cityData.forEach(function(city) {
   .addTo(map);
 });
 
-// Coordinates for each point to be used in the polyline.
-let line = [
-  [33.9416, -118.4085],
-  [37.6213, -122.3790],
-  [40.7899, -111.9791],
-  [47.4502, -122.3088]
-];
-
-// Create a polyline using the line coordinates and make the line yellow.
-L.polyline(line, {
-  color: "yellow"
-}).addTo(map);
 
 // Create the tile layer that will be the background of the map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
